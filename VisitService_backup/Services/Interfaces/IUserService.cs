253121b00ec955cd186d95bfa8a3fafcd05c175a
benayanoone1 <benayan@one1.co.il>
2025-09-14 18:34:10 +0300

    remove backup

--- VisitService_backup/Services/Interfaces/IUserService.cs
+++
@@ -1,9 +0,0 @@
-using VisitService.Models;
-
-namespace VisitService.Services.Interfaces;
-
-public interface IUserService
-{
-    Task<UsersPaginated> GetUsers(int skip, int limit, UserFilter filters, CancellationToken cancellationToken);
-    Task<User>  GetUserById(string userId,CancellationToken cancellationToken);
-}
